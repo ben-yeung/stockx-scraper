9537af3e314b8ce5a34c83b489e6b0a90aa16ced
Ben Yeung <benjaminyeung@berkeley.edu>
2022-03-24 18:46:44 -0700

    Added support for non-sized products

--- index.js
+++ index.js
@@ -5,77 +5,128 @@ const  secrets = require("./secrets.json")
 
 // Format for key:val is query:{size entries}
 // Size entries are size:target
+// Target price is evaluted on last sale
 // Leave val to be target price if no size specified
 let queries = {
-    "yeezy foam rnnr stone sage":{9:200, 13:200}
+    "yeezy foam rnnr stone sage":{9:100, 13:200},
+    "kaws family figure black":100
 }
 
 const webhook = secrets["WEBHOOK"];
 
 async function getStockXPrices() {
     for (const [key, val] of Object.entries(queries)) {
-        stockX.searchProducts(key).then((queryResults) => {
-            stockX.fetchProductDetails(queryResults[0]).then((productResults) => {
-                if (typeof(val) == "number") {
-
-                } else if (Object.keys(val).length != 0) {
-                    for (const [size, target] of Object.entries(val)) {
-                        try {
-                            stockX.fetchProductDetails(productResults.sizeMap[size.toString()]).then((sizeDetails) => {
-                                console.log(sizeDetails)
-                                if (sizeDetails.market["lastSale"] > target) {
-                                    console.log(`Target price met for size ${size}`);
-                                    let sourceURL = `https://stockx.com/${sizeDetails.market.productUuid}?size=${size}`
-                                    const formattedData = `[Link To StockX](${sourceURL})\n`
-                                    embed = {
-                                        "title":"**Size " + size + " | " + sizeDetails["name"] + "**",
-                                        "color":25408,
-                                        "thumbnail":{
-                                            "url":sizeDetails["image"]
-                                        },
-                                        "fields": [
-                                            {
-                                                "name":"Target Price",
-                                                "value":`$${target}`,
-                                                "inline":true
-                                            },
-                                            {
-                                                "name":"Lowest Ask",
-                                                "value":`$${sizeDetails.market["lowestAsk"]}`,
-                                                "inline":true
-                                            },
-                                            {
-                                                "name":"Last Sale",
-                                                "value":`$${sizeDetails.market["lastSale"]}`,
-                                                "inline":true
-                                            },
-                                        ],
-                                        "description":formattedData
-                                    }
-                        
-                                    fetch(webhook, {
-                                        "method":"POST",
-                                        "headers": {
-                                            'Content-Type': 'application/json',
-                                        },
-                                        body: JSON.stringify({
-                                            "username":"StockX Scraper",
-                                            "embeds":[embed],
-                                            "avatar_url":"https://pbs.twimg.com/profile_images/1382744393594064901/_s7Up6o__400x400.jpg"
-                                        })
-                                    })
-                                }
+        try {
+            stockX.searchProducts(key).then((queryResults) => {
+                stockX.fetchProductDetails(queryResults[0]).then((productResults) => {
+                    if (typeof(val) == "number") {
+                        if (productResults.market["lastSale"] > val) {
+                            let sourceURL = `https://stockx.com/${productResults.market.productUuid}`;
+                            const formattedData = `[Link To StockX](${sourceURL})\n`;
+                            const timestamp = new Date().toISOString();
+                            embed = {
+                                "title":"**" + productResults["name"] + "**",
+                                "color":25408,
+                                "thumbnail":{
+                                    "url":productResults["image"]
+                                },
+                                "fields": [
+                                    {
+                                        "name":"Target Price",
+                                        "value":`$${val}`,
+                                        "inline":true
+                                    },
+                                    {
+                                        "name":"Lowest Ask",
+                                        "value":`$${productResults.market["lowestAsk"]}`,
+                                        "inline":true
+                                    },
+                                    {
+                                        "name":"Last Sale",
+                                        "value":`$${productResults.market["lastSale"]}`,
+                                        "inline":true
+                                    },
+                                ],
+                                "description":formattedData,
+                                "timestamp":timestamp
+                            }
+                
+                            fetch(webhook, {
+                                "method":"POST",
+                                "headers": {
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify({
+                                    "username":"StockX Scraper",
+                                    "embeds":[embed],
+                                    "avatar_url":"https://pbs.twimg.com/profile_images/1382744393594064901/_s7Up6o__400x400.jpg"
+                                })
                             })
-                        } catch (e) {
-                            console.log("ERROR: Fetching size details")
                         }
-                        
+    
+                    } else if (Object.keys(val).length != 0) {
+                        for (const [size, target] of Object.entries(val)) {
+                            try {
+                                stockX.fetchProductDetails(productResults.sizeMap[size.toString()]).then((sizeDetails) => {
+                                    // console.log(sizeDetails)
+                                    if (sizeDetails.market["lastSale"] > target) {
+                                        let sourceURL = `https://stockx.com/${sizeDetails.market.productUuid}?size=${size}`;
+                                        const formattedData = `[Link To StockX](${sourceURL})\n`;
+                                        const timestamp = new Date().toISOString();
+                                        embed = {
+                                            "title":"**Size " + size + " | " + sizeDetails["name"] + "**",
+                                            "color":25408,
+                                            "thumbnail":{
+                                                "url":sizeDetails["image"]
+                                            },
+                                            "fields": [
+                                                {
+                                                    "name":"Target Price",
+                                                    "value":`$${target}`,
+                                                    "inline":true
+                                                },
+                                                {
+                                                    "name":"Lowest Ask",
+                                                    "value":`$${sizeDetails.market["lowestAsk"]}`,
+                                                    "inline":true
+                                                },
+                                                {
+                                                    "name":"Last Sale",
+                                                    "value":`$${sizeDetails.market["lastSale"]}`,
+                                                    "inline":true
+                                                },
+                                            ],
+                                            "description":formattedData,
+                                            "timestamp":timestamp
+                                        }
+                            
+                                        fetch(webhook, {
+                                            "method":"POST",
+                                            "headers": {
+                                                'Content-Type': 'application/json',
+                                            },
+                                            body: JSON.stringify({
+                                                "username":"StockX Scraper",
+                                                "embeds":[embed],
+                                                "avatar_url":"https://pbs.twimg.com/profile_images/1382744393594064901/_s7Up6o__400x400.jpg"
+                                            })
+                                        })
+                                    }
+                                })
+                            } catch (e) {
+                                console.log("ERROR: Fetching size details");
+                            }
+                            
+                        }
+                    } else { // Error check for invalid input
+                        console.log("ERROR parsing inputted query");
                     }
-                } else { // No sizes specified
-
-                }
+                })
             })
-        })
+        } catch (e) {
+            console.log("ERROR fetching product double check queries object.");
+        }
+        
     }
 }
 getStockXPrices()
